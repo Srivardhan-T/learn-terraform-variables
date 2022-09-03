e45bd51ef124a1481578939a965135498052640b
Srivardhan-T <awssasri@gmail.com>
2022-09-03 16:27:35 +0530

    added variables

--- variables.tf
+++ variables.tf
@@ -1,1 +1,25 @@
 # Variable declarations
+
+variable "aws_region" {
+    description = "AWS region"
+    type = string
+    default = us-east-1
+}
+
+variable "vpc_cidr_block"
+    description = "CIDR block for vpc"
+    type = string
+    default = 10.0.0.0/16
+
+variable "instance_discount" {
+    description = "Number of instances provision"
+    type = number
+    default = 2 
+}
+
+
+variable "enable_vpn_gateway" {
+    description = "Enable a VPN gateway in your VPC."
+    type = bool
+    default = false
+}
